66edde62291e512e64663a7a40fe815a1846ccc4
Eric_2139 <ericpowercosta2005@gmail.com>
2023-12-06 17:13:12 -0300

    atualização

--- 3-Opçoes/script.js
+++ 3-Opçoes/script.js
@@ -1,7 +1,7 @@
 function encaminhar_jogo() {
-    window.location.href = 'http://127.0.0.1:5500/5-Jogo-Math-Scape/index.html'
+    window.location.href = 'http://127.0.0.1:5500/5-Jogo%20Math%20Scape/index.html'
 }
 
 function encaminhar_criador() {
-    window.location.href = 'http://127.0.0.1:5500/4-Criador-de-fases/index4.html'
+    window.location.href = 'http://127.0.0.1:5500/4-Criador%20de%20fases/index4.html'
 }
